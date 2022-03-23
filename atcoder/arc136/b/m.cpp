3c5bc398d3ea3dc0b576a9c35d8f85c0d0e47047
KakeruKitahara <takusan315@gmail.com>
2022-03-23 09:22:10 +0900

    finabc244

--- atcoder/arc136/b/m.cpp
+++ atcoder/arc136/b/m.cpp
@@ -5,7 +5,7 @@ using namespace atcoder;
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n ; a <= i; i--)
+#define REPR2(i, n, a) for (int i = n; a <= i; i--)
 using V = vector<int>;
 using Vl = vector<long long>;
 using VV = vector<vector<int>>;
@@ -17,64 +17,102 @@ using ll = long long;
 constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
- 
-
-// 1-indexedなので注意。
-struct BIT
-{
-private:
-  vector<int> bit;
-  int N;
-
-public:
-  BIT(int size)
-  {
-    N = size;
-    bit.resize(N + 1);
-  }
-
-  // 一点更新です
-  void add(int a, int w)
-  {
-    for (int x = a; x <= N; x += x & -x)
-      bit[x] += w;
-  }
-
-  // 1~Nまでの和を求める。
-  int sum(int a)
-  {
-    int ret = 0;
-    for (int x = a; x > 0; x -= x & -x)
-      ret += bit[x];
-    return ret;
-  }
-};
-
-// ====================================================================
 
 int main()
 {
   int n;
   cin >> n;
-  vector<int> v(n - 2), v(n) ;
-  for (int i = 0; i < n; i++)
+  V a(n), b(n);
+  V ca(5001), cb(5001);
+
+  REP(i, n)
   {
-    cin >> v[i];
+    cin >> a[i];
+    ca[a[i]]++;
   }
-
-  REP(i, n -2){
-
-  }
-
-  ll ans = 0;
-  BIT b(n); // これまでの数字がどんな風になってるのかをメモる為のBIT
-  for (int i = 0; i < n; i++)
+  REP(i, n)
   {
-    ans += i - b.sum(v[i]); // BITの総和 - 自分より左側 ＝ 自分より右側
-    b.add(v[i], 1);         // 自分の位置に1を足す(ダジャレではないです)
+    cin >> b[i];
+    cb[b[i]]++;
   }
 
-  if(ans % 2 == 0){
-    if()
+  REP2(i, 1, 5001)
+  {
+    if (ca[i] != cb[i])
+    {
+      cout << "No" << endl;
+      return 0;
+    }
   }
+
+  REP2(i, 1, 5001)
+  {
+    if (2 <= ca[i])
+    {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  unordered_map<int, int> ma, mb;
+  int j = 0;
+
+  REP2(i, 1, 5001)
+  {
+    if (ca[i] == 1)
+    {
+      ma[i] = j;
+      j++;
+    }
+  }
+
+  j = 0;
+  REP2(i, 1, 5001)
+  {
+    if (cb[i] == 1)
+    {
+      mb[i] = j;
+      j++;
+    }
+  }
+
+  REP(i, n)
+  {
+    a[i] = ma[a[i]];
+    b[i] = mb[b[i]];
+  }
+
+  dsu da(n), db(n);
+  set<int> sa, sb;
+  REP(i, n)
+  {
+    da.merge(i, a[i]);
+  }
+
+  REP(i, n)
+  {
+    sa.insert(da.leader(i));
+  }
+
+  REP(i, n)
+  {
+    db.merge(i, b[i]);
+  }
+
+  REP(i, n)
+  {
+    sb.insert(db.leader(i));
+  }
+
+  int circlea = sa.size(), circleb = sb.size();
+
+  if (circlea % 2 == circleb % 2)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+  return 0;
 }
