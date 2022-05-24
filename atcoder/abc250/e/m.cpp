67720d1f94a7e087e77ee6000d96e95b60fb8b8a
KakeruKitahara <takusan315@gmail.com>
2022-05-24 16:00:56 +0900

    [add] abc250e, arc140b(WA)

--- atcoder/abc250/e/m.cpp
+++ atcoder/abc250/e/m.cpp
@@ -18,40 +18,101 @@ constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
 
+uint32_t xor128(void)
+{
+  static uint32_t x = 123456789;
+  static uint32_t y = 362436069;
+  static uint32_t z = 521288629;
+  static uint32_t w = 88675123;
+  uint32_t t;
+
+  t = x ^ (x << 11);
+  x = y;
+  y = z;
+  z = w;
+  return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+}
+
 int main()
 {
+
   int n;
   cin >> n;
-  V a(n);
-  V b(n);
-
+  Vl a(n), b(n);
   REP(i, n)
   {
     cin >> a[i];
   }
-
   REP(i, n)
   {
     cin >> b[i];
   }
-
   int q;
   cin >> q;
-  V x(q), y(q);
 
-  REP(i, n)
+  vector<P> xy(q);
+
+  REP(i, q)
   {
-    cin >> x[i] >> y[i];
-    x[i]--;
-    y[i]--;
+    cin >> xy[i].first >> xy[i].second;
   }
 
-  VV dp(n, V(n));
+  set<ll> o;
   REP(i, n)
   {
-    REP(i, n){
-      
+    o.insert(a[i]);
+    o.insert(b[i]);
+  }
+
+  map<ll, ll> tra, tra2;
+
+  for (int i : o)
+  {
+    tra[i] = xor128();
+  }
+
+  tra2 = tra;
+
+  REP(i, n)
+  {
+    int t1 = a[i], t2 = b[i];
+    a[i] = tra[a[i]];
+    b[i] = tra2[b[i]];
+    tra[t1] = -1;
+    tra2[t2] = -1;
+  }
+
+  Vl suma(n), sumb(n);
+
+  REP(i, n)
+  {
+    if (i == 0)
+    {
+      suma[i] = a[i];
+      sumb[i] = b[i];
+      continue;
+    }
+    if (a[i] != -1)
+      suma[i] = suma[i - 1] ^ a[i];
+    else
+      suma[i] = suma[i - 1];
+    if (b[i] != -1)
+      sumb[i] = sumb[i - 1] ^ b[i];
+    else
+      sumb[i] = sumb[i - 1];
+  }
+
+  REP(i, q)
+  {
+    if (suma[xy[i].first - 1] == sumb[xy[i].second - 1])
+    {
+      cout << "Yes" << endl;
+    }
+    else
+    {
+      cout << "No" << endl;
     }
   }
+
   return 0;
 }
