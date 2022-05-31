d102aa7aef9900f51394ff503a8ad66343974d66
KakeruKitahara <takusan315@gmail.com>
2022-05-31 16:32:24 +0900

    [add] abc200. abc235.

--- atcoder/abc200/d/m.cpp
+++ atcoder/abc200/d/m.cpp
@@ -5,7 +5,7 @@ using namespace atcoder;
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n ; a <= i; i--)
+#define REPR2(i, n, a) for (int i = n; a <= i; i--)
 using V = vector<int>;
 using Vl = vector<long long>;
 using VV = vector<vector<int>>;
@@ -17,8 +17,76 @@ using ll = long long;
 constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
- 
-int main(){
 
-return 0;
+int main()
+{
+  int n;
+  cin >> n;
+  V a(n);
+  REP(i, n)
+  {
+    int c;
+    cin >> c;
+    a[i] = c % 200;
+  }
+
+  multiset<pair<int, V>> bc;
+
+  for (int bit = 0; bit < (1 << min(n, 9)); bit++)
+  {
+    V t;
+    int sum = 0;
+    if (bit != 0)
+    {
+      for (int i = 0; i < min(n, 9); i++)
+      {
+        if (bit & (1 << i))
+        {
+          t.push_back(i);
+          sum += a[i];
+          sum %= 200;
+        }
+      }
+      bc.insert(make_pair(sum, t));
+    }
+  }
+  auto last = bc.end();
+  last--;
+
+  V b, c;
+
+  for (auto it = bc.begin(); it != last; it++)
+  {
+    auto it2 = it;
+    it2++;
+    if (it->first == it2->first)
+    {
+      b = it->second;
+      c = it2->second;
+      break;
+    }
+  }
+
+  if (b.size() == 0 && c.size() == 0)
+  {
+    cout << "No" << endl;
+  }
+  else
+  {
+    cout << "Yes" << endl;
+    cout << b.size() << " ";
+    REP(i, b.size())
+    {
+      cout << b[i] + 1 << " ";
+    }
+    cout << endl;
+    cout << c.size() << " ";
+    REP(i, c.size())
+    {
+      cout << c[i] + 1 << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
 }
--- atcoder/abc235/d/m.cpp
+++ atcoder/abc235/d/m.cpp
@@ -5,7 +5,7 @@ using namespace atcoder;
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n ; a <= i; i--)
+#define REPR2(i, n, a) for (int i = n; a <= i; i--)
 using V = vector<int>;
 using Vl = vector<long long>;
 using VV = vector<vector<int>>;
@@ -17,9 +17,74 @@ using ll = long long;
 constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
- 
-int main(){
-  
+int pow(int a, int b)
+{
+  int sum = 1;
+  REP(i, b)
+  {
+    sum *= a;
+  }
 
-return 0;
+  return sum;
+}
+
+int main()
+{
+  int a, n;
+  cin >> a >> n;
+  int x = 1;
+  map<ll, int> cnt;
+
+  int n2 = n;
+  int c2 = 0;
+
+  while (n2 != 0)
+  {
+    c2++;
+    n2 /= 10;
+  }
+
+  queue<tuple<int, int, int>> q;
+  tuple<int, int, int> ans = make_tuple(-1, -1, -1);
+  q.push(make_tuple(0, x, -1));
+
+  while (q.size() != 0)
+  {
+    tuple<int, int, int> ptr = q.front();
+    q.pop();
+
+    if (get<1>(ptr) == n)
+    {
+      ans = ptr;
+      break;
+    }
+
+    int pp = get<1>(ptr);
+    int c = 0;
+    while (pp != 0)
+    {
+      c++;
+      pp /= 10;
+    }
+
+    if (c2 >= c)
+    {
+      q.push(make_tuple(get<0>(ptr) + 1, get<1>(ptr) * a, 0));
+      cnt[get<1>(ptr)] = 1;
+    }
+    int d = get<1>(ptr) % 10;
+    if (d != 0)
+    {
+      get<1>(ptr) = get<1>(ptr) / 10 + pow(10, c - 1) * d;
+      if (cnt.count(get<1>(ptr)) == 0)
+      {
+        q.push(make_tuple(get<0>(ptr) + 1, get<1>(ptr), 1));
+        cnt[get<1>(ptr)] = 1;
+      }
+    }
+  }
+
+  cout << get<0>(ans) << endl;
+
+  return 0;
 }
