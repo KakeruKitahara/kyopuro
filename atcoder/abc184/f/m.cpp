e4dc1be91e888213ab84ba04f3c169e62c15ea28
Pythagoras-theorem <takusan315@gmail.com>
2022-07-28 00:38:33 +0900

    [add] abc184, ty42

--- atcoder/abc184/f/m.cpp
+++ atcoder/abc184/f/m.cpp
@@ -2,23 +2,112 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
+
+/* macro */
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n ; a <= i; i--)
 using V = vector<int>;
-using Vl = vector<long long>;
 using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
 using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
 using P = pair<int, int>;
-using Pl = pair<long long, long long>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
 using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
 using ll = long long;
 constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
- 
-int main(){
 
-return 0;
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+int main()
+{
+  int n, t;
+  cin >> n >> t;
+  VV a(2);
+  REP(i, n / 2)
+  {
+    int aa;
+    cin >> aa;
+    a[0].push_back(aa);
+  }
+  REP2(i, n / 2, n)
+  {
+    int aa;
+    cin >> aa;
+    a[1].push_back(aa);
+  }
+  VVl tmp(2);
+  REP(j, 2)
+  {
+    for (int bit = 0; bit < (1 << a[j].size()); bit++) // 1 << n = 2^n通り.
+    {
+      ll sum = 0;
+      for (int i = 0; i < a[j].size(); i++)
+      {
+        if (bit & (1 << i))
+        {
+          sum += a[j][i];
+        }
+      }
+
+      tmp[j].push_back(sum);
+    }
+  }
+
+  sort(tmp[1].begin(), tmp[1].end());
+  ll ans = -1;
+  REP(i, tmp[0].size())
+  {
+    ll tt = tmp[0][i] + *(upper_bound(tmp[1].begin(), tmp[1].end(), t - tmp[0][i]) - 1);
+    if (tt <= t)
+    {
+      ans = max(ans, tt);
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
 }
