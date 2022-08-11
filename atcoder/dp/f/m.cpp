7b4d629276c88eebdf78198b5f2194edd8393092
Pythagoras-theorem <takusan315@gmail.com>
2022-08-11 01:31:33 +0000

    [add] dp f

--- atcoder/dp/f/m.cpp
+++ atcoder/dp/f/m.cpp
@@ -63,5 +63,108 @@ pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
 
 int main()
 {
+  S s, t;
+  cin >> s >> t;
+
+  VV dp(s.size(), V(t.size()));
+
+  if (s[0] == t[0])
+  {
+    dp[0][0] = 1;
+  }
+
+  REP2(i, 1, t.size())
+  {
+    if (s[0] == t[i])
+    {
+      dp[0][i] = 1;
+    }
+    else
+    {
+      dp[0][i] = dp[0][i - 1];
+    }
+  }
+
+  REP2(i, 1, s.size())
+  {
+    if (s[i] == t[0])
+    {
+      dp[i][0] = 1;
+    }
+    else
+    {
+      dp[i][0] = dp[i - 1][0];
+    }
+  }
+
+  REP2(i, 1, s.size())
+  {
+    REP2(j, 1, t.size())
+    {
+      if (s[i] == t[j])
+      {
+        dp[i][j] = max(dp[i][j - 1], max(dp[i - 1][j], dp[i - 1][j - 1] + 1));
+      }
+      else
+      {
+        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+  }
+
+  int k = t.size() - 1;
+  S ans;
+  int i = s.size() - 1, j = t.size() - 1;
+  while (dp[i][j] != 0)
+  {
+    if (i == 0 && j == 0)
+    {
+      if (s[0] == t[0])
+      {
+        ans.push_back(s[0]);
+      }
+      break;
+    }
+    else if (i == 0)
+    {
+      if (dp[0][j] == dp[0][j - 1] + 1)
+      {
+        ans.push_back(s[0]);
+      }
+      j = j - 1;
+      continue;
+    }
+    else if (j == 0)
+    {
+      if (dp[i][0] == dp[i - 1][0] + 1)
+      {
+        ans.push_back(t[0]);
+      }
+
+      i = i - 1;
+      continue;
+    }
+    if (dp[i][j] == dp[i - 1][j - 1] + 1 && s[i] == t[j])
+    {
+      ans.push_back(s[i]);
+      j = j - 1;
+      i = i - 1;
+    }
+    else if (0 < j && dp[i][j] == dp[i][j - 1])
+    {
+      j = j - 1;
+    }
+    else if (0 < i && dp[i][j] == dp[i - 1][j])
+    {
+      i = i - 1;
+    }
+  }
+
+  REPR(i, ans.size())
+  {
+    cout << ans[i];
+  }
+  cout << endl;
+
   return 0;
 }
