e3d1bf0cf6d2a22676577714243d347e0f96a469
root <takusan315@gmail.com>
2022-09-27 16:57:33 +0900

    [add] abc127

---
+++ atcoder/abc127/d/m.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+/* macro */
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP2(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
+using V = vector<int>;
+using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
+using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
+using P = pair<int, int>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
+using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
+using ll = long long;
+constexpr int IINF = 1000000000 + 8;
+constexpr long long LINF = 1000000000000000000LL + 8;
+using mint = modint1000000007;
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+
+  map<int, ll> a;
+
+  REP(i, n)
+  {
+    int aa;
+    cin >> aa;
+    a[aa]++;
+  }
+
+  V b(m), c(m);
+
+  REP(i, m)
+  {
+    cin >> b[i] >> c[i];
+    auto it = a.upper_bound(c[i]);
+
+    V de;
+
+    int cnt = 0;
+    for (auto p = a.begin(); it != p; p++)
+    {
+      cnt += p->second;
+      if (cnt > b[i])
+      {
+
+        p->second -= b[i] - (cnt - p->second);
+        cnt = b[i];
+        break;
+      }
+      p->second = 0;
+      de.push_back(p->first);
+    }
+
+    REP(i, de.size())
+    {
+      a.erase(de[i]);
+    }
+
+    a[c[i]] += cnt;
+  }
+  ll sum = 0;
+  for (auto k : a)
+  {
+    sum += k.second * k.first;
+  }
+
+  cout << sum << endl;
+
+  return 0;
+}
