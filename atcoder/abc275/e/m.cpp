dc2236338407abeac5b8a8fd2cdd283bfb62a99d
root <takusan315@gmail.com>
2022-11-01 17:13:34 +0900

    [add] abc275

---
+++ atcoder/abc275/e/m.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+/* macro */
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP2(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
+using V = vector<int>;
+using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
+using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
+using P = pair<int, int>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
+using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
+using ll = long long;
+constexpr int IINF = 1000000000 + 8;
+constexpr long long LINF = 1000000000000000000LL + 8;
+using mint = modint998244353;
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+int main()
+{
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  vector<mint> predp(n + 1);
+  vector<mint> dp(n + 1);
+  mint ans = 1;
+  REP(i, k)
+  {
+    ans *= m;
+    predp.clear();
+    predp.resize(n + 1);
+    swap(dp, predp);
+
+    if (i == 0)
+    {
+      REP2(l, 1, m + 1)
+      {
+        dp[l] = 1;
+      }
+      continue;
+    }
+    REP(j, n + 1)
+    {
+      REP2(l, 1, m + 1)
+      {
+        if (j + l > n)
+        {
+          dp[2 * n - (j + l)] += predp[j];
+        }
+        else
+        {
+          dp[j + l] += predp[j];
+        }
+      }
+    }
+  }
+
+  ans = ans.inv();
+  ans *= dp[n];
+
+  cout << ans.val() << endl;
+
+  return 0;
+}
