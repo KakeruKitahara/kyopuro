df1386a5d3758404b8a900c34d4e15b15430eef3
root <takusan315@gmail.com>
2022-11-22 16:58:05 +0900

    [add] abc278

---
+++ atcoder/abc278/c/m.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+/* macro */
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP2(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
+using V = vector<int>;
+using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
+using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
+using P = pair<int, int>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
+using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
+using ll = long long;
+constexpr int IINF = 1000000000 + 8;
+constexpr long long LINF = 1000000000000000000LL + 8;
+using mint = modint1000000007;
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+int main()
+{
+  int n, q;
+  cin >> n >> q;
+  V t(q), a(q), b(q);
+
+  REP(i, q)
+  {
+    cin >> t[i] >> a[i] >> b[i];
+  }
+
+  map<P, int> mp;
+
+  REP(i, q)
+  {
+    P p = make_pair(a[i], b[i]);
+    if (t[i] == 1)
+    {
+      mp[p] = 1;
+    }
+    else if (t[i] == 2)
+    {
+      mp[p] = 0;
+    }
+    else
+    {
+      if (mp[p] == 1 && mp[make_pair(b[i], a[i])] == 1)
+      {
+        cout << "Yes" << endl;
+      }
+      else
+      {
+        cout << "No" << endl;
+      }
+    }
+  }
+  return 0;
+}
