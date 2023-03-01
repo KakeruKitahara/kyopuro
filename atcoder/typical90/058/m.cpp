85fa8af0f82f93900853d0919499c237cf1500da
root <takusan315@gmail.com>
2023-03-01 15:04:34 +0900

    [add] typical 16, 58, 85

--- atcoder/typical90/058/m.cpp
+++ atcoder/typical90/058/m.cpp
@@ -2,23 +2,246 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
+
+/* macro */
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n ; a <= i; i--)
 using V = vector<int>;
-using Vl = vector<long long>;
 using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
 using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
 using P = pair<int, int>;
 using Pl = pair<long long, long long>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
 using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
 using ll = long long;
-constexpr int INF = 1000000000 + 8;
+constexpr int IINF = 1000000000 + 8;
+constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
- 
-int main(){
-  
 
-return 0;
+/* change function */
+template <class X>
+void chmin(X &a, X b) { a = min(a, b); }
+template <class X>
+void chmax(X &a, X b) { a = max(a, b); }
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+/* Euclid  */
+template <class X>
+X gcd(X a, X b)
+{
+  if (b == 0)
+    return a;
+  else
+    return gcd(b, a % b);
+}
+
+/* pow  */
+template <class X>
+X pow(X x, X n)
+{
+  X ret = 1;
+  while (n > 0)
+  {
+    if (n & 1)
+      ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+/* fenick tree */
+class fenick
+{
+  vector<ll> bit;
+  fenick(int n)
+  {
+    bit.resize(n);
+  }
+
+public:
+  void add(int i, ll x)
+  {
+    i++;
+    for (int idx = i; idx <= bit.size(); idx += (idx & -idx))
+    {
+      bit[idx - 1] += x;
+    }
+  }
+
+  ll sum(int i)
+  {
+    ll ans = 0;
+    for (int idx = i; idx > 0; idx -= (idx & -idx))
+    {
+      ans += bit[idx - 1];
+    }
+    return ans;
+  }
+
+  ll sum(int a, int b)
+  {
+    return sum(b) - sum(a - 1);
+  }
+};
+
+/* dikstra */
+void dijkstra(VVP graph, int s, Vl &dis)
+{
+  dis.resize(graph.size(), LINF);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    { // first : to, second : cost
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        pq.emplace(dis[e.first], e.second);
+      }
+    }
+  }
+}
+
+void dijkstra(VVP graph, int s, Vl dis, V &prev)
+{
+  dis.resize(graph.size(), LINF);
+  prev.resize(graph.size(), -1);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    {
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        prev[e.first] = v;
+        pq.emplace(dis[e.first], e.second);
+      }
+    }
+  }
+}
+
+V get_dikstra_path(const V prev, int t)
+{
+  V path;
+  for (int cur = t; cur != -1; cur = prev[cur])
+  {
+    path.push_back(cur);
+  }
+  reverse(path.begin(), path.end());
+  return path;
+}
+
+int mod;
+
+int f(int n)
+{
+  int sum = 0;
+  int n2 = n;
+  while (n2 != 0)
+  {
+    sum += n2 % 10;
+    n2 /= 10;
+  }
+  return (sum + n) % mod;
+}
+
+int main()
+{
+  mod = 100000;
+  int n;
+  ll k;
+  cin >> n >> k;
+  V dp(mod, -1);
+  dp[n] = 0;
+  int cnt = 0;
+  REP(i, IINF)
+  {
+    cnt++;
+    n = f(n);
+    if (cnt == k)
+    {
+      cout << n << endl;
+      return 0;
+    }
+    if (dp[n] == -1)
+    {
+      dp[n] = cnt;
+    }
+    else
+    {
+      break;
+    }
+  }
+  k -= dp[n];
+  cnt -= dp[n];
+  int a  = k % cnt;
+  REP(i, a)
+  {
+    n = f(n);
+  }
+  cout << n << endl;
+  return 0;
 }
---
+++ atcoder/typical90/085/m2.cpp
@@ -0,0 +1,270 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+/* macro */
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP2(i, a, n) for (int i = a; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
+using V = vector<int>;
+using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
+using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
+using P = pair<int, int>;
+using Pl = pair<long long, long long>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
+using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
+using ll = long long;
+constexpr int IINF = 1000000000 + 8;
+constexpr long long LINF = 1000000000000000000LL + 8;
+using mint = modint1000000007;
+
+/* change function */
+template <class X>
+void chmin(X &a, X b) { a = min(a, b); }
+template <class X>
+void chmax(X &a, X b) { a = max(a, b); }
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+/* Euclid  */
+template <class X>
+X gcd(X a, X b)
+{
+  if (b == 0)
+    return a;
+  else
+    return gcd(b, a % b);
+}
+
+/* pow  */
+template <class X>
+X pow(X x, X n)
+{
+  X ret = 1;
+  while (n > 0)
+  {
+    if (n & 1)
+      ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+/* fenick tree */
+class fenick
+{
+  vector<ll> bit;
+  fenick(int n)
+  {
+    bit.resize(n);
+  }
+
+public:
+  void add(int i, ll x)
+  {
+    i++;
+    for (int idx = i; idx <= bit.size(); idx += (idx & -idx))
+    {
+      bit[idx - 1] += x;
+    }
+  }
+
+  ll sum(int i)
+  {
+    ll ans = 0;
+    for (int idx = i; idx > 0; idx -= (idx & -idx))
+    {
+      ans += bit[idx - 1];
+    }
+    return ans;
+  }
+
+  ll sum(int a, int b)
+  {
+    return sum(b) - sum(a - 1);
+  }
+};
+
+/* dikstra */
+void dijkstra(VVP graph, int s, Vl &dis)
+{
+  dis.resize(graph.size(), LINF);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    { // first : to, second : cost
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        pq.emplace(dis[e.first], e.second);
+      }
+    }
+  }
+}
+
+void dijkstra(VVP graph, int s, Vl dis, V &prev)
+{
+  dis.resize(graph.size(), LINF);
+  prev.resize(graph.size(), -1);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    {
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        prev[e.first] = v;
+        pq.emplace(dis[e.first], e.second);
+      }
+    }
+  }
+}
+
+V get_dikstra_path(const V prev, int t)
+{
+  V path;
+  for (int cur = t; cur != -1; cur = prev[cur])
+  {
+    path.push_back(cur);
+  }
+  reverse(path.begin(), path.end());
+  return path;
+}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<pair<long long, long long>> prime_factorize(long long N)
+{
+  vector<pair<long long, long long>> res;
+
+  for (long long p = 2; p * p <= N; ++p)
+  {
+    if (N % p != 0)
+    {
+      continue;
+    }
+
+    int e = 0;
+    while (N % p == 0)
+    {
+      ++e;
+
+      N /= p;
+    }
+
+    res.emplace_back(p, e);
+  }
+
+  if (N != 1)
+  {
+    res.emplace_back(N, 1);
+  }
+  return res;
+}
+
+vector<long long> calc_divisors(long long N)
+{
+  vector<long long> res;
+  for (long long i = 1; i * i <= N; ++i)
+  {
+    if (N % i != 0)
+      continue;
+
+    res.push_back(i);
+
+    if (N / i != i)
+      res.push_back(N / i);
+  }
+  sort(res.begin(), res.end());
+  return res;
+}
+
+int main()
+{
+  ll k;
+  cin >> k;
+
+  Vl a = calc_divisors(k);
+
+  int ans = 0;
+  REP(i, a.size())
+  {
+    Vl b = calc_divisors(k / a[i]);
+    REP(j, b.size())
+    {
+      ll c = k / b[j] / a[i];
+      if (a[i] <= b[j] && b[j] <= c)
+      {
+        ans++;
+      }
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
