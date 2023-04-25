8c477e222b9034a72d7f6596d270fae0e55007a5
root <takusan315@gmail.com>
2023-04-25 16:06:27 +0900

    [add] abc248 e

--- atcoder/abc248/e/m.cpp
+++ atcoder/abc248/e/m.cpp
@@ -2,21 +2,197 @@
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
+
+/* macro */
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REP2(i, a, n) for (int i = a; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; 0 <= i; i--)
-#define REPR2(i, n, a) for (int i = n; a <= i; i--)
 using V = vector<int>;
-using Vl = vector<long long>;
 using VV = vector<vector<int>>;
+using VVV = vector<vector<vector<int>>>;
+using Vl = vector<long long>;
 using VVl = vector<vector<long long>>;
+using VVVl = vector<vector<vector<long long>>>;
 using P = pair<int, int>;
 using Pl = pair<long long, long long>;
+using VP = vector<pair<int, int>>;
+using VVP = vector<vector<pair<int, int>>>;
 using S = string;
+using VS = vector<string>;
+using VVS = vector<vector<string>>;
 using ll = long long;
 constexpr int IINF = 1000000000 + 8;
 constexpr long long LINF = 1000000000000000000LL + 8;
 using mint = modint1000000007;
+
+/* change function */
+template <class X>
+void chmin(X &a, X b) { a = min(a, b); }
+template <class X>
+void chmax(X &a, X b) { a = max(a, b); }
+
+/* pair operator */
+template <class X>
+pair<X, X> operator+(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first + ob2.first;
+  res.second = ob1.second + ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator-(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first - ob2.first;
+  res.second = ob1.second - ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator*(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first * ob2.first;
+  res.second = ob1.second * ob2.second;
+  return res;
+}
+
+template <class X>
+pair<X, X> operator/(pair<X, X> &ob1, pair<X, X> &ob2)
+{
+  pair<X, X> res;
+  res.first = ob1.first / ob2.first;
+  res.second = ob1.second / ob2.second;
+  return res;
+}
+
+/* Euclid  */
+template <class X>
+X gcd(X a, X b)
+{
+  if (b == 0)
+    return a;
+  else
+    return gcd(b, a % b);
+}
+
+/* pow  */
+template <class X>
+X pow(X x, X n)
+{
+  X ret = 1;
+  while (n > 0)
+  {
+    if (n & 1)
+      ret *= x;
+    x *= x;
+    n >>= 1;
+  }
+  return ret;
+}
+
+/* fenick tree */
+class fenick
+{
+  vector<ll> bit;
+  fenick(int n)
+  {
+    bit.resize(n);
+  }
+
+public:
+  void add(int i, ll x)
+  {
+    i++;
+    for (int idx = i; idx <= bit.size(); idx += (idx & -idx))
+    {
+      bit[idx - 1] += x;
+    }
+  }
+
+  ll sum(int i)
+  {
+    ll ans = 0;
+    for (int idx = i; idx > 0; idx -= (idx & -idx))
+    {
+      ans += bit[idx - 1];
+    }
+    return ans;
+  }
+
+  ll sum(int a, int b)
+  {
+    return sum(b) - sum(a - 1);
+  }
+};
+
+/* dikstra */
+void dijkstra(VVP graph, int s, Vl &dis)
+{
+  dis.resize(graph.size(), LINF);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    { // first : to, second : cost
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        pq.emplace(dis[e.first], e.first);
+      }
+    }
+  }
+}
+
+void dijkstra(VVP graph, int s, Vl dis, V &prev)
+{
+  dis.resize(graph.size(), LINF);
+  prev.resize(graph.size(), -1);
+  priority_queue<Pl, vector<Pl>, greater<Pl>> pq;
+  dis[s] = 0;
+  pq.emplace(dis[s], s);
+  while (!pq.empty())
+  {
+    P p = pq.top();
+    pq.pop();
+    int v = p.second;
+    if (dis[v] < p.first)
+    {
+      continue;
+    }
+    for (auto &e : graph[v])
+    {
+      if (dis[e.first] > dis[v] + e.second)
+      {
+        dis[e.first] = dis[v] + e.second;
+        prev[e.first] = v;
+        pq.emplace(dis[e.first], e.second);
+      }
+    }
+  }
+}
+
+V get_dikstra_path(const V prev, int t)
+{
+  V path;
+  for (int cur = t; cur != -1; cur = prev[cur])
+  {
+    path.push_back(cur);
+  }
+  reverse(path.begin(), path.end());
+  return path;
+}
 
 int main()
 {
@@ -27,43 +203,75 @@ int main()
   {
     cin >> x[i] >> y[i];
   }
+
   if (k == 1)
   {
     cout << "Infinity" << endl;
     return 0;
   }
-  int ans = 0;
-
+  set<set<int>> cnt;
   REP(i, n)
   {
-    REP2(j, i + 1, n)
+    P p = {x[i], y[i]};
+    map<P, set<int>> grad;
+
+    REP(j, n)
     {
-      int cnt = 0;
-      REP(l, n)
+      if (i == j)
+        continue;
+      P q = {x[j], y[j]};
+      P gr;
+      gr.first = p.first - q.first;
+      if (gr.first < 0)
       {
-        if (i == l || j == l){
-          if(i == l && k == 2){
-            ans++;
-          }
-          continue;
-        }
-        if (min(x[i], x[j]) <= x[l] && x[l] <= max(x[i], x[j]))
-        {
-          if (min(y[i], y[j]) <= y[l] && y[l] <= max(y[i], y[j]))
-          {
-            if ((x[i] - x[l]) * (y[l] - y[j]) == (x[l] - x[j]) * (y[i] - y[l]))
-            {
-              cnt++;
-            }
-          }
-        }
+        gr.first = -gr.first;
+        gr.second = y[j] - y[i];
       }
-      if(k == cnt){
-        ans++;
+      else if (gr.first == 0)
+      {
+        gr.second = 1;
       }
+      else
+      {
+        gr.second = y[i] - y[j];
+      }
+
+      if (gr.second == 0)
+      {
+        gr.first = 1;
+      }
+
+      if (!(gr.first == 0 && gr.second == 0))
+      {
+        int g = gcd(abs(gr.first), abs(gr.second));
+        gr.first /= g;
+        gr.second /= g;
+      }
+
+      if (grad.count(gr) == 0)
+      {
+        grad[gr].insert(i);
+      }
+      grad[gr].insert(j);
+    }
+
+    for (auto it : grad)
+    {
+      cnt.insert(it.second);
+    }
+
+    grad.clear();
+  }
+
+  int ans = 0;
+  for (auto it : cnt)
+  {
+    if (it.size() >= k)
+    {
+      ans++;
     }
   }
-  cout << ans << endl;
 
+  cout << ans << endl;
   return 0;
 }
